9ec44ae7a1934569163a663e74e30a565440af4d
Khaldoun Halawani <khalawani@sadasol.com>
2024-12-03 13:23:37 +0200

    JS 11: Todo 2 (logic done)

--- dom/todo/script.js
+++ dom/todo/script.js
@@ -1,30 +1,70 @@
 const inputEle = document.getElementById("new-todo");
 const todosEle = document.getElementById("todos");
-let items = [];
+const doneEle = document.getElementById("done");
 
-const renderItems = () => {
+// const EMPTY_ITEM_TEMPLATE = {
+//     id: Date.now(),
+//     title: value,
+//     done: false
+// }
+
+const storeData = () => {
+    localStorage.setItem('todos', JSON.stringify(items));
+}
+
+const readData = () => {
+    try {
+        const strJSON = localStorage.getItem('todos');
+        return strJSON === null ? [] : JSON.parse(strJSON);
+
+    } catch (error) {
+        return [];
+    }
+}
+
+const renderItems = (rItems = items) => {
     todosEle.innerHTML = "";
-    items.forEach((item, index) => {
-        todosEle.insertAdjacentHTML('beforeend', `
+    doneEle.innerHTML = "";
+    rItems.forEach((item, index) => {
+        const strEl = `
         <li>
-            <input type="checkbox">
-            <p>${index + 1}. ${item}</p>
-            <div>
-            <button onclick="deleteItem(${index})">❌</button>
-            <button>✏</button>
-            </div>
-        </li>`);
+        <input type="checkbox" onchange="toggle(${index})" ${item.done ? 'checked' : ''}>
+        <p>${index + 1}. ${item.title}</p>
+        <div>
+        <button onclick="deleteItem(${index})">❌</button>
+        ${item.done ? '' : `<button onclick="edit(${index})">✏</button>`}
+        </div>
+        </li>`;
+
+        if (!item.done) {
+            todosEle.insertAdjacentHTML('beforeend', strEl);
+        } else {
+            doneEle.insertAdjacentHTML('beforeend', strEl);
+        }
     })
 }
 
-const addItem = () => {
+let items = readData();
+renderItems();
+
+const addItem = (event) => {
+    if (event && event.key !== "Enter") {
+        return;
+    }
+
     let value = "";
     value = inputEle.value;
     if (value.length > 0) {    // validation
-        items.push(value)
+        const newItem = {
+            id: Date.now(),
+            title: value,
+            done: false
+        }
+        items.push(newItem)
         inputEle.value = "";
         renderItems();
     }
+    storeData();
 }
 
 const deleteItem = (index) => {
@@ -34,5 +74,27 @@ const deleteItem = (index) => {
     //     if (item === value) return false;
     //     else return true;
     // });
+    renderItems();
+    storeData();
+}
+
+const toggle = (index) => {
+    items[index].done = !items[index].done;
+    renderItems();
+    storeData();
+}
+
+const search = (event) => {
+    const term = event.target.value.toLowerCase();
+    if (term.length >= 3) {
+        const filteredItems = items.filter(item => item.title.toLowerCase().includes(term));
+        renderItems(filteredItems);
+    } else {
+        renderItems();
+    }
+}
+
+const edit = (index) => {
+    items[index].title = prompt("Please enter new title", items[index].title) || items[index].title;
     renderItems();
 }
