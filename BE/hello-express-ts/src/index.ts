5a9d566ee1f3be37d9fdc0d32f7bf6431b9d10fc
Khaldoun Halawani <khalawani@sadasol.com>
2025-05-13 13:47:59 +0300

    Express 4: Status Codes

--- BE/hello-express-ts/src/index.ts
+++ BE/hello-express-ts/src/index.ts
@@ -3,8 +3,23 @@ import express from 'express';
 const app = express()
 const port: number = 3000;
 
+app.get('/users', (req: express.Request, res: express.Response) => {
+  try {
+    // Read all users from
+    // throw ("error");
+    
+    res.status(401).send("You are unauthorized to do this!")
+  } catch (error) {
+    res.status(500).send("Failed to find users");
+  }
+});
+
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+  res.status(204).send();
+});
+
+app.all('', (req: express.Request, res: express.Response) => {
+  res.status(404).send("Not found!");
 });
 
 app.listen(port, () => {
